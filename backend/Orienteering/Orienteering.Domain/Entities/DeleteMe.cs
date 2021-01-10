c8ee9ac4825f7982d3c6f22a3dc79c1d1246b436
mantasmak <maksimaviciaus@gmail.com>
2021-01-11 00:05:56 +0100

    Add Domain project.

---
+++ backend/Orienteering/Orienteering.Domain/Entities/DeleteMe.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Orienteering.Domain.Entities
+{
+    class DeleteMe
+    {
+    }
+}
--- backend/Orienteering/Orienteering.Domain/Interfaces/DeleteMe.cs
+++ backend/Orienteering/Orienteering.Domain/Interfaces/DeleteMe.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Orienteering.Infrastructure.Persistence.Migrations
+namespace Orienteering.Domain.Interfaces
 {
     class DeleteMe
     {

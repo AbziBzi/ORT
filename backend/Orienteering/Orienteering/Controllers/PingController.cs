fb22f7d7a986a54d758bfe718dd25c9fea528a51
mantasmak <maksimaviciaus@gmail.com>
2021-01-10 19:06:18 +0100

    Add folder template.

---
+++ backend/Orienteering/Orienteering/Controllers/PingController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Orienteering.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        // GET: api/<PingController>
+        [HttpGet]
+        public string Get()
+        {
+            return "Ping";
+        }
+    }
+}
---
+++ backend/Orienteering/Orienteering/Resources/DeleteMe.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Orienteering.Web.Resources
+{
+    public class DeleteMe
+    {
+    }
+}
